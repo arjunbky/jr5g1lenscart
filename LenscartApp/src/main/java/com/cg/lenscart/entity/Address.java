7d0086fbe6bfc28d3dfb93dae296c81dbe4c7d6a
Anu <Anu@DESKTOP-041HCF2>
2021-04-02 13:37:33 +0530

    address updated

--- LenscartApp/src/main/java/com/cg/lenscart/entity/Address.java
+++ LenscartApp/src/main/java/com/cg/lenscart/entity/Address.java
@@ -1,6 +1,36 @@
 package com.cg.lenscart.entity;
+import java.io.Serializable;
 
-public class Address {
-	
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="Address")
+public class Address  implements Serializable    { 
+
+@Column(name=" House_number",length=5)
+private String House_number;
+@Column (name=" Street_name",length=20)
+private String Street_name;
+@Column (name="City ",length=20)
+private String City;
+@Column(name="State",length=20)
+private String State;
+@Column(name="Pincode",length=6)
+private int Pincode;
+public Address () {
+		
+	}
+public  Address(String House_number, String Street_name, String City, String State, int Pincode)  {
+ super();
+this. House_number=House_number;
+this. Street_name= Street_name;
+this .City =  City;
+this.State = State;
+this.Pincode=Pincode ;
+}
 }
