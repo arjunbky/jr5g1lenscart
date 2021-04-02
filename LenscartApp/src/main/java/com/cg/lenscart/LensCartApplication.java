4566ca405f8ec500d5c502d1ed925eadd480116e
arjun <arjun@ARJUN>
2021-04-02 13:22:57 +0530

    application

--- LenscartApp/src/main/java/com/cg/lenscart/LensCartApplication.java
+++ LenscartApp/src/main/java/com/cg/lenscart/LensCartApplication.java
@@ -1,13 +1,13 @@
 package com.cg.lenscart;
 
-public class LensCartApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class LensCartApplication {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//testing push from another computer
-		//TEST
-		
-System.out.println("hello hi");
+		SpringApplication.run(LensCartApplication.class, args);
+
 	}
 
 }
