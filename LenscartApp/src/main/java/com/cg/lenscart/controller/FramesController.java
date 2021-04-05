74aecdc94e4d8df3f3f2735a5cf1a305c4750360
arjun <arjun@DESKTOP-809PA2M>
2021-04-05 10:40:06 +0530

    afafd

--- LenscartApp/src/main/java/com/cg/lenscart/controller/FramesController.java
+++ LenscartApp/src/main/java/com/cg/lenscart/controller/FramesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cg.lenscart.dto.FramesDto;
 import com.cg.lenscart.entity.Frames;
 import com.cg.lenscart.services.FramesService;
 
@@ -25,8 +26,8 @@ public class FramesController {
 		FramesService service;
 		
 		@PostMapping("/insertframes")
-		public ResponseEntity<String> addFrames(@RequestBody Frames frames){
-			service.addFrames(frames);
+		public ResponseEntity<String> addFrames(@RequestBody FramesDto dto){
+			service.addFrames(dto);
 			return new ResponseEntity<String>("Frames Added",HttpStatus.OK);
 		}
 		@GetMapping
